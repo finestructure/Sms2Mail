be9d310d9097079119149fb50644d5270ac7258c
sas <sas@bfea69f6-0cce-0310-9b16-88a7a1f2502c>
2008-08-31 19:25:12 +0000

    New version sporting cache and AddressBook lookup.
    git-svn-id: https://dev.abstracture.de/repos/Projects/sas/sms2mail@3536 bfea69f6-0cce-0310-9b16-88a7a1f2502c

--- sms2mail.py
+++ sms2mail.py
@@ -13,8 +13,11 @@ import tempfile
 import ConfigParser
 import imaplib
 import time
+import pickle
+import AddressBook
 
-config_filename = '~/.sms2mail.conf'
+config_filename = os.path.expanduser('~/.sms2mail.conf')
+cache_filename = os.path.expanduser('~/.sms2mail.cache')
 
 
 def getSmsPlist(backupdir):
@@ -44,7 +47,7 @@ def getSqliteFile(backupdir):
     return temp
 
 
-def getGroups(connection):
+def getGroups(connection, phonebook):
     """
     Get the groups from the sqlite database.
     """
@@ -54,6 +57,11 @@ def getGroups(connection):
     cursor.execute('select group_id, address '
                    'from group_member order by address')
     for g, a in cursor.fetchall():
+        a = filter_digits(a)
+        try:
+            a = phonebook[a]
+        except KeyError:
+            pass
         if g not in groups.keys():
             groups[g] = [a]
         else:
@@ -72,7 +80,14 @@ def createMessages(connection, mynumber, filter=None):
     time: Timestamp of the sms message in seconds since the epoch.
     message: The sms text message body.
     """
-    groups = getGroups(conn)
+    phonebook = getPhonebook()
+    groups = getGroups(conn, phonebook)
+    
+    mynumber = filter_digits(mynumber)
+    try:
+        me = phonebook[mynumber]
+    except KeyError:
+        me = mynumber
 
     cursor = connection.cursor()
 
@@ -84,17 +99,24 @@ def createMessages(connection, mynumber, filter=None):
             continue
         if filter is not None and filter not in a:
             continue
+        
+        a = filter_digits(a)
+        try:
+            other = phonebook[a]
+        except KeyError:
+            other = a
+        
         subject = ('Subject: Conversation with '
                    '%s (Group %d)') % (', '.join(groups[g]), g)
         if f == 2: # to me
-            sender = 'From: ' + a
-            recipient = 'To: ' + mynumber
+            sender = 'From: %s' % other
+            recipient = 'To: %s' % me
         elif f == 3: # from me
-            sender = 'From: ' + mynumber
-            recipient = 'To: ' + a
+            sender = 'From: %s' % me
+            recipient = 'To: %s' % other
         else: # default (seen flags 2050 from voice box)
-            sender = 'From: ' + a
-            recipient = 'To: ' + mynumber
+            sender = 'From: %s' % other
+            recipient = 'To: %s' % me
         date = 'Date: %s' % datetime.datetime.fromtimestamp(d)
         sms_id = hashlib.sha1('%s%d%d%d' % (a,d,f,g)).hexdigest()
         sms_id_header = 'Sms-Id: ' + sms_id
@@ -111,12 +133,22 @@ def createMessages(connection, mynumber, filter=None):
     return messages            
 
 
+def readExistingIdsCache():
+    if not os.path.exists(cache_filename):
+        return set()
+    else:
+        cache = open(cache_filename)
+        res = pickle.load(cache)
+        cache.close()
+        return res
+
+
 def getExistingIds(imap_connection):
     """
     Gets the lists of sms ids already on the IMAP server. Every message
     has a header 'Sms-Id' with the unique text message hash.
     """
-    existing_ids = []
+    existing_ids = set()
     
     t, data = imap_connection.search(None, 'ALL')
     for num in data[0].split():
@@ -124,7 +156,7 @@ def getExistingIds(imap_connection):
         msgtext = data[0][1]
         for line in msgtext.split('\r\n'):
             if line.startswith('Sms-Id:'):
-                existing_ids.append(line.split()[1])
+                existing_ids.add(line.split()[1])
     
     return existing_ids
 
@@ -135,20 +167,87 @@ def uploadMessages(messages):
     their sms_id. Only messages with ids not already present on the server
     are uploaded.
     """
+    cached_ids = readExistingIdsCache()
+    # check if there are any new messags at all against the cache
+    # before hitting the IMAP server
+    for msg in messages:
+        if msg['id'] in cached_ids:
+            messages.remove(msg)
+    if messages == []:
+        return
+    
+    # there are messages left which are not in our cache, refresh it now
     M = imaplib.IMAP4_SSL(host=host, port=port)
     M.login(user, password)    
     M.select(sms_mailbox)
 
-    existing_ids = getExistingIds(M)
-    
+    # unite both sets (cache and refreshed ones) and save them in the cache
+    # the reason we don't simply overwrite the cache is that this way if a
+    # user has deleted sms mails they won't be resynched -- this will only
+    # happen when the cache is manually reset or the script is run from 
+    # another machine
+    server_ids = getExistingIds(M)
+    existing_ids = cached_ids.union(server_ids)
+    cache = open(cache_filename, 'w')
+    pickle.dump(existing_ids, cache)
+    cache.close()
+        
     for msg in messages:
         if msg['id'] not in existing_ids:
             flags = ''
             date = time.localtime(msg['time'])
-            print M.append(sms_mailbox, flags, date, msg['message'])
+            M.append(sms_mailbox, flags, date, msg['message'])
 
     M.close()
     M.logout()
+
+
+def filter_digits(string):
+    digits = '0123456789'
+    return ''.join([c for c in string if c in digits])
+
+
+# shamelessly stolen from the PyObjC example shipping w/ Xcode and adapted
+
+
+def encodeField(value):
+    """
+    Encode a value into an UTF-8 string
+    """
+    if value is None:
+        return ''
+
+    if isinstance(value, AddressBook.ABMultiValue):
+        # A multi-valued property, merge them into a single string
+        result = []
+        for i in range(len(value)):
+            result.append(value.valueAtIndex_(i).encode('utf-8'))
+        return result
+
+    return value.encode('utf-8')
+
+
+def getPhonebook():
+    """
+    Creat a 'phonebook', i.e. a dictionary {'123456':'John Doe'}.
+    The reason we're doing this instead of using the AB search API is that
+    the API has no way to suppress spaces and extra characters like dashes,
+    slashes etc. If an entry has a phone number '49 171 123456' it will not
+    show up when searching for '49171123456'. (Quite strange that, the
+    AddressBook application ignores whitespace, it must use another mechanism.)
+    """
+    phonebook = {}
+    book = AddressBook.ABGetSharedAddressBook()
+    fields = ( AddressBook.kABPhoneProperty,
+               AddressBook.kABFirstNameProperty,
+               AddressBook.kABLastNameProperty)
+
+    for person in book.people():
+        record = [ encodeField(person.valueForProperty_(i)) for i in fields ]
+        for phonenumber in [filter_digits(i) for i in record[0]]:
+            phonebook[phonenumber] = ' '.join([record[1], record[2]])
+    return phonebook
+
 
 
 if __name__ == '__main__':
@@ -159,7 +258,7 @@ if __name__ == '__main__':
 
     # get options    
     config = ConfigParser.ConfigParser()
-    config.read(os.path.expanduser(config_filename))
+    config.read(config_filename)
     
     user = config.get('IMAP', 'user')
     password = config.get('IMAP', 'password')
