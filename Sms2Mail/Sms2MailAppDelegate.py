5ad1e20438abb3c5eea9e642d671cd0e45353da0
sas <sas@bfea69f6-0cce-0310-9b16-88a7a1f2502c>
2009-12-05 18:08:41 +0000

    Added product type mapping.
    git-svn-id: https://dev.abstracture.de/repos/Projects/sas/sms2mail@6662 bfea69f6-0cce-0310-9b16-88a7a1f2502c

--- Sms2Mail/Sms2MailAppDelegate.py
+++ Sms2Mail/Sms2MailAppDelegate.py
@@ -13,14 +13,25 @@ import sms2mail
 class Sms2MailAppDelegate(NSObject):
 
   tf = objc.IBOutlet()
+  productTypes = {'iPhone1,1':'Original iPhone',
+                  'iPhone1,2':'iPhone 3G',
+                  'iPhone2,1':'iPhone 3GS',
+                  'iPod1,1':'Original iPod touch',
+                  'iPod2,1':'iPod touch (2nd gen)',
+                  'iPod3,1':'iPod touch (3rd gen)',
+                  }
 
   def applicationDidFinishLaunching_(self, sender):
     self.devices = sms2mail.listDevices()
     self.tf.removeAllItems()
     for dev in self.devices:
+      try:
+        productType = self.productTypes[dev['Product Type']]
+      except KeyError:
+        productType = dev['Product Type']
       title = '%s (%s - %s)' % (dev['Device Name'], 
                                 dev['Product Version'],
-                                dev['Product Type'])
+                                productType)
       self.tf.addItemWithTitle_(title)
       
   @objc.IBAction
