ecf33d551f798ae92358bab727a38d25569c9eab
sas <sas@bfea69f6-0cce-0310-9b16-88a7a1f2502c>
2009-12-16 12:01:53 +0000

    Showing serial number and accessing message list.
    git-svn-id: https://dev.abstracture.de/repos/Projects/sas/sms2mail@6710 bfea69f6-0cce-0310-9b16-88a7a1f2502c

--- Sms2Mail/Sms2MailAppDelegate.py
+++ Sms2Mail/Sms2MailAppDelegate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 #  Sms2MailAppDelegate.py
 #  Sms2Mail
@@ -9,38 +10,55 @@
 from Foundation import *
 from AppKit import *
 import sms2mail
+import sqlite3
 
 class Sms2MailAppDelegate(NSObject):
 
   devicePopup = objc.IBOutlet()
   productVersionLabel = objc.IBOutlet()
   lastBackupDateLabel = objc.IBOutlet()
+  serialNumberLabel = objc.IBOutlet()
+  
   productTypes = {'iPhone1,1':'Original iPhone',
                   'iPhone1,2':'iPhone 3G',
                   'iPhone2,1':'iPhone 3GS',
                   'iPod1,1':'Original iPod touch',
-                  'iPod2,1':'iPod touch (2nd gen)',
-                  'iPod3,1':'iPod touch (3rd gen)',
+                  'iPod2,1':'2nd gen iPod touch',
+                  'iPod3,1':'3rd gen iPod touch',
                   }
 
   def applicationDidFinishLaunching_(self, sender):
     self.devices = sms2mail.listDevices()
     self.devicePopup.removeAllItems()
     for dev in self.devices:
-      print 'dev:', dev
       try:
         productType = self.productTypes[dev['Product Type']]
       except KeyError:
         productType = dev['Product Type']
-      title = '%s (%s)' % (dev['Device Name'], productType)
+      title = u'%s – %s (%s)' % (dev['Device Name'], 
+                                productType,
+                                dev['Serial Number'])
       self.devicePopup.addItemWithTitle_(title)
-    self.productVersionLabel.setObjectValue_(self.devices[0]['Product Version'])
-    self.lastBackupDateLabel.setObjectValue_(self.devices[0]['Last Backup Date'])
-      
+    self.selectDevice(self.devices[0])
+
+
   @objc.IBAction
   def popupSelected_(self, sender):
-    NSLog('popup selected: ' + str(sender.indexOfSelectedItem()))
     dev = self.devices[self.devicePopup.indexOfSelectedItem()]
-    self.productVersionLabel.setValue_(dev['Product Version'])
-    self.lastBackupDateLabel.setValue_(dev['Last Backup Date'])
-    
+    self.selectDevice(dev)
+  
+  
+  def selectDevice(self, device):
+    self.productVersionLabel.setObjectValue_(device['Product Version'])
+    self.lastBackupDateLabel.setObjectValue_(device['Last Backup Date'])
+    self.serialNumberLabel.setObjectValue_(device['Serial Number'])
+    sqlitefile, fname = sms2mail.getSqliteFile(device['Backup Directory'])
+    print '... found:', fname
+    print 'Connecting with sqlite3 ...'
+    sqlitedb = sqlite3.connect(sqlitefile.name)
+    print '... successful.'
+    print 'Getting messages from sqlite db ...'
+    messages = sms2mail.getMessages(sqlitedb, device['Phone Number'])
+    print '... found %d messages' % len(messages)
+
+  
